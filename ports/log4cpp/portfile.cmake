bb1f9105c72b469c3032f9cc51eaff679f793c20
Tom Burdick <thomas.burdick@gmail.com>
2021-05-14 13:44:58 -0500

    Began porting libs to vcpkg for easier dev builds

---
+++ ports/log4cpp/portfile.cmake
@@ -0,0 +1,20 @@
+vcpkg_from_github (
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO openenergysolutions/log4cpp
+    REF 96c716b0d3b4d6921df4bb77d7749aaaeee19dfe 
+    SHA512 f6e712c45a2d378870078b2cf17f6eab4ca9b062aa6fcd284353fcc7e6556ed2ad2e91178f68442297a1b717ce591926590256a830bc203cd3c7955649543185
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+#file(
+#    INSTALL "${SOURCE_PATH}/LICENSE"
+#    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+#    RENAME copyright)
